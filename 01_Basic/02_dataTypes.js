e6d7bc475df9f821c75afaa9700059ac9e47bfb3
Akash Bhargava <99540132+technophyyl@users.noreply.github.com>
2024-04-06 07:10:57 +0000

    dataType understanding

---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+console.log(3+3);
+console.log("Akash'");
+
+let name = "Akash"
+let age = 19
+let isLoggedIn = false
+let state = null
+
+//number => 2 to poer 53
+//bigint
+//string =>""
+//null => standalone vslue
+//undefined =>
+//symbol => unique 
+
+//object
+
+console.log(typeof "Akash");
+console.log(typeof null);
+
+
