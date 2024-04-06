38501cf263b91bcaebe9a12f005e951899edd821
Akash Bhargava <99540132+technophyyl@users.noreply.github.com>
2024-04-06 07:10:34 +0000

    variables understanding

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,11 @@
+const accountId =144883
+let accountEmail= "akash@meta.com"
+var accountPassword ="India@12345"
+accountCity ="Gurugram"
+
+accountEmail= "aakashb@gmail.com"
+accountPassword = "12345";
+accountCity = "Indore"
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountPassword,accountCity])
